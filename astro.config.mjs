6a4a335090608a0fd38c62ff2e8960f0a5ac4ca2
DominicDev <s0l0estudi4r@gmail.com>
2025-04-20 15:30:42 +0200

    v0.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +9,5 @@ export default defineConfig({
     inlineStylesheets: 'always'
   },
 
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  integrations: [tailwind()]
 });
